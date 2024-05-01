ac6af81fb3e132cd12eda333be6fb0dc7d926f8c
Pedro Willen <pedrownsmcpe@gmail.com>
2024-05-01 01:28:23 -0400

    Adding function spreadImages

---
+++ scripts/script-interface.js
@@ -0,0 +1,46 @@
+let images = [
+    'images/bootstrap.png',
+    'images/css.png',
+    'images/electron.png',
+    'images/firebase.png',
+    'images/html.png',
+    'images/javascript.png',
+    'images/jquery.png',
+    'images/mongo.png',
+    'images/node.png',
+    'images/react.png',
+    'images/bootstrap.png',
+    'images/css.png',
+    'images/electron.png',
+    'images/firebase.png',
+    'images/html.png',
+    'images/javascript.png',
+    'images/jquery.png',
+    'images/mongo.png',
+    'images/node.png',
+    'images/react.png',
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    spreadImages(images);
+})
+
+function generateRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+}
+
+function spreadImages(images) {
+    let cardsBack = document.querySelectorAll('.card-back');
+    let selectedIndex = [];
+
+    while (selectedIndex.length < images.length) {
+        let randomIndex = generateRandomIndex(images);
+        if(!selectedIndex.includes(randomIndex)) {
+            selectedIndex.push(randomIndex);
+        }
+    }
+
+    for(let i = 0; i < cardsBack.length; i++) {
+        cardsBack[i].innerHTML = `<img src="${images[selectedIndex[i]]}">`
+    }
+}
