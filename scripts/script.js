b865993673c7ac5aad371cdce407e53a8886c37e
Pedro Willen <pedrownsmcpe@gmail.com>
2024-05-01 00:13:45 -0400

    Adding id for each card

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     let cards = document.querySelectorAll('.card');
+    let cardsBack = document.querySelectorAll('.card-back');
+    
+    for(var i = 0; i < cardsBack.length; i++) {
+        cardsBack[i].setAttribute('id', `${i}`)
+    }
 
-    cards.forEach(card => card.addEventListener('click', handleMove))
+    cards.forEach(card => card.addEventListener('click', handleMove));
 })
 
-function handleMove () {
-    
+function handleMove (event) {
+    console.log(event.target)
 }
