818ccc2e1e60676bfae96f896733396cf8f3cbab
Pedro Willen <pedrownsmcpe@gmail.com>
2024-05-03 14:28:47 -0400

    Improving code

--- scripts/script-interface.js
+++ scripts/script-interface.js
@@ -1,25 +1,15 @@
 let images = [
-    'images/bootstrap.png',
-    'images/css.png',
-    'images/electron.png',
-    'images/firebase.png',
-    'images/html.png',
-    'images/javascript.png',
-    'images/jquery.png',
-    'images/mongo.png',
-    'images/node.png',
-    'images/react.png',
-    'images/bootstrap.png',
-    'images/css.png',
-    'images/electron.png',
-    'images/firebase.png',
-    'images/html.png',
-    'images/javascript.png',
-    'images/jquery.png',
-    'images/mongo.png',
-    'images/node.png',
-    'images/react.png',
-];
+    'bootstrap',
+    'css',
+    'electron',
+    'firebase',
+    'html',
+    'javascript',
+    'jquery',
+    'mongo',
+    'node',
+    'react'
+]
 
 document.addEventListener('DOMContentLoaded', () => {
     spreadImages(images);
@@ -31,17 +21,18 @@ function generateRandomIndex(array) {
 
 function spreadImages(images) {
     let cardsBack = document.querySelectorAll('.card-back');
+    images.forEach(img => images.push(img));
     let selectedIndex = [];
 
     //generate random number and verify if exists
     while (selectedIndex.length < images.length) {
         let randomIndex = generateRandomIndex(images);
-        if(!selectedIndex.includes(randomIndex)) {
+        if (!selectedIndex.includes(randomIndex)) {
             selectedIndex.push(randomIndex);
         }
     }
-    
-    for(let i = 0; i < cardsBack.length; i++) {
-        cardsBack[i].innerHTML = `<img src="${images[selectedIndex[i]]}">`
+
+    for (let i = 0; i < cardsBack.length; i++) {
+        cardsBack[i].innerHTML = `<img src="images/${images[selectedIndex[i]]}.png">`
     }
 }
